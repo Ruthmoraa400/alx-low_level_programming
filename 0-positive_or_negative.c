73185039fd9a897bba8b401ca56873480b207f1c
Ruthmoraa400 <nyangau.ruth@yahoo.com>
2021-11-20 12:08:57 -0800

    0x01-variables_if_else_while

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <-Wall -Werror -Wextra -pedantic -std=gnu89.h>
+
+"linter-betty"
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+    int a = 0, b = 1, c = -1;
+
+    if (b > a) {  
+    printf("positive");
+	
+    else (c < a) {
+	printf("negative");
+	
+	}
+  
+}
+	return (0);
+}
